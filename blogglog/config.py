19876c9e3a2968a671a3b7068a15f133c48c4520
eddy <eddyyonnie@gmail.com>
2019-04-30 12:42:06 +0300

    styled the comment page

---
+++ blogglog/config.py
@@ -0,0 +1,37 @@
+import os
+
+class Config:
+    SECRET_KEY=os.environ.get('SECRET_KEY')
+    
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringa:12345@localhost/pitch'
+    MAIL_SERVER ='smtp.googlemail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
+    MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
+    UPLOADED_PHOTOS_DEST = 'app/static/photos'
+
+    SUBJECT_PREFIX = 'One Minute Pitch'
+    SENDER_EMAIL = 'eddyyonnie@gmail.com'
+
+    @staticmethod
+    def init_app(app):
+        pass
+
+class ProdConfig(Config):
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
+      
+
+
+class TestConfig(Config):
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringa:12345@localhost/pitch'
+
+class DevConfig(Config):
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringa:12345@localhost/pitch'
+    DEBUG = True
+    
+config_options = {
+    'development':DevConfig,
+    'production':ProdConfig,
+    'test':TestConfig
+}
